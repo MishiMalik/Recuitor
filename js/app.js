5b76896e1617f2002e5cd07702b79cffb4534862
mehwish <mehwishgul715@gmail.com>
2022-01-16 23:20:32 +0500

    Manage Jobs

--- js/app.js
+++ js/app.js
@@ -148,31 +148,28 @@ function updateFormSteps() {
     formSteps[formStepsNum].classList.add("step-forms-active");
 }
 
-// function updateProgressbar() {
-//     progressSteps.forEach((progressStep, idx) => {
-//         if (idx < formStepsNum + 1) { 
-//             progressStep.classList.add("progress-step-active"); 
-//         } 
-//         else { 
-//             progressStep.classList.remove("progress-step-active"); 
-//         }
-//     }); 
-//     progressSteps.forEach((progressStep, idx) => {
-//         if (idx < formStepsNum) {
-//          progressStep.classList.add("progress-step-check"); 
-//         } else { 
-//         progressStep.classList.remove("progress-step-check"); }
-//     }); 
-//     const progressActive = document.querySelectorAll(".progress-step-active"); 
-//     progress.style.width = ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
-// }
-//  document.getElementById("submit-form").addEventListener("click", function () {
-//     progressSteps.forEach((progressStep, idx) => {
-//         if (idx <= formStepsNum) { 
-//             progressStep.classList.add("progress-step-check"); 
-//         } else { 
-//             progressStep.classList.remove("progress-step-check");
-//         }
-//     });
-//      var forms = document.getElementById("forms"); forms.classList.remove("form"); forms.innerHTML = '<div class="welcome"><div class="content"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><h2>Thanks for signup!</h2><span>We will contact you soon!</span><div></div>';
-// });
+
+
+// 
+
+$(".gallery__cards  ").addClass("portfolio__projects grid");
+
+let btn = $('.state__list');
+
+btn.click(function (e) {
+    $('.state__list button').removeClass("active-port");
+    e.target.classList.add("active-port");
+
+    let selector = $(e.target).attr('data-filter');
+    $('.portfolio .grid').isotope({
+        filter: selector
+    });
+    return false;
+});
+// $('.gallery__cards').isotope({ layoutMode: 'vertical' })
+var $grid = $('.gallery__cards').isotope({
+    // main isotope options
+    itemSelector: '.gallery__cards-item',
+    // set layoutMode
+    layoutMode: 'vertical'
+  })
